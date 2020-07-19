704c239d35332ef7f547f1c35b89c6a170a3a8c5
priyanka amuthalakshmi <japriyanka95@gmail.com>
2020-07-19 22:41:31 +1000

    Updates

---
+++ precache-manifest.b5d7349443510321a8ed5951661557cf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46464a2d9c6c59899d8470fadc28376b",
+    "url": "/covid-tracker/index.html"
+  },
+  {
+    "revision": "f39877aca53a42e1f7bc",
+    "url": "/covid-tracker/static/css/main.ad39fd54.chunk.css"
+  },
+  {
+    "revision": "009c2581c4a34b7e6274",
+    "url": "/covid-tracker/static/js/2.aaf9c2bd.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid-tracker/static/js/2.aaf9c2bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f39877aca53a42e1f7bc",
+    "url": "/covid-tracker/static/js/main.3e39ce47.chunk.js"
+  },
+  {
+    "revision": "a2c09be945803fa375fb",
+    "url": "/covid-tracker/static/js/runtime-main.948b6ba1.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid-tracker/static/media/covidimage.d7265326.png"
+  }
+]);
